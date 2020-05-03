2f8309911c54587e6aad8fcd10f937c0553f07ad
RobertoLRV <61212477+RobertoLRV@users.noreply.github.com>
2020-05-03 19:00:25 -0300

    Implementação do MVC

---
+++ Aula7/src/service/PaisService.java
@@ -0,0 +1,30 @@
+package service;
+import dao.PaisDAO;
+import model.Pais;
+
+public class PaisService {
+	PaisDAO dao = new PaisDAO();
+	
+	public  int criar(Pais pais) {
+		return dao.criar(pais);
+	}
+	public void atualizar(Pais pais) {
+		dao.atualizar(pais);
+	}
+	public void excluir(int id) {
+		dao.excluir(id);
+	}
+	public Pais carregar(int id) {
+		Pais pais = dao.carregar(id);
+		return pais;
+	}
+	public Pais MaiorP() {
+		return dao.maiorpopulacao();
+	}
+	public Pais MenorA() {
+		return dao.menorArea();
+	}
+	public Pais[] Vetor() {
+		return dao.Vetor1();
+	}
+}
